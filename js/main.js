38278b117fd974e7f2c2f2f9f8e52a7e177a4a63
Roman0410 <100242100+Roman0410@users.noreply.github.com>
2023-09-03 23:08:21 +0300

    ++

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".header-burger").click(function (event) {
+  $(".header-nav").toggleClass("active");
+  $(".header-burger").toggleClass("active");
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (event) {
+    event.preventDefault();
+    const blockID = anchor.getAttribute("href");
+    document.querySelector("" + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
